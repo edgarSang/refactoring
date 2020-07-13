5e71696e0d586f22c4678ced8dcfba8e092d2121
HanSangHa <hansh01@lotte.net>
2020-07-13 16:10:25 +0900

    format 단위변화함수 usd 로 구체화

--- src/ch1/common.js
+++ src/ch1/common.js
@@ -8,17 +8,22 @@ function statement(invoice) {
   let totalAmout = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {style:"currency", currency: "USD", minimumFractionDigits: 2}).format;
   for (let perf of invoice.performances) {
     volumeCredits == volumeCreditsFor(perf);
 
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf, playFor(perf))/100)} (${perf.audience}석)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf, playFor(perf)))} (${perf.audience}석)\n`;
     totalAmout += amountFor(perf, playFor(perf));
   }
 
-  result += `총액: ${format(totalAmout/100)}\n`;
+  result += `총액: ${usd(totalAmout)}\n`;
   result += `적립 포인트 : ${volumeCredits}점 \n`;
   return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US",
+                   {style:"currency", currency: "USD",
+                    minimumFractionDigits: 2}).format(aNumber/100);
 }
 
 function volumeCreditsFor(aPerformance) {
