ee2e74b61d178f0fdf7f4ce68f68a7e3c1243449
HanSangHa <hansh01@lotte.net>
2020-07-14 14:13:49 +0900

    amount calculator로 대체

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -13,7 +13,7 @@ export default function createStatementData(invoice, plays) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance)); // 공연정보를 인자로
     const result = Object.assign({},aPerformance);
     result.play = playFor(result);
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
@@ -23,26 +23,7 @@ export default function createStatementData(invoice, plays) {
   }
 
   function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if(aPerformance.audience > 30) {
-          result += 10000 *(aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if(aPerformance.audience > 20) {
-          result += 10000 + 500 *(aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르 ${aPerformance.play.type}`);
-    }
-  
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
 
   function volumeCreditsFor(aPerformance) {
