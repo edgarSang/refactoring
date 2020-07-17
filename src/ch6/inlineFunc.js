e9028332a15e42bd363d9cafa393130604175e21
HanSangHa <hansh01@lotte.net>
2020-07-17 14:47:30 +0900

    함수 인라인 단계별로

--- src/ch6/inlineFunc.js
+++ src/ch6/inlineFunc.js
@@ -7,11 +7,12 @@ function rating(aDriver) {
 // inline 2 좀 더 복잡한 경우
 function reportLines(aCustomer) {
   const lines = [];
+  lines.push(['name', aCustomer.name]);
   gatherCustomerData(lines, aCustomer);
   return lines;
 }
 
 function gatherCustomerData(out, aCustomer) {
-  out.push(["name", aCustomer.name]);
+  // out.push(["name", aCustomer.name]);
   out.push(["location", aCustomer.location]);
 }
