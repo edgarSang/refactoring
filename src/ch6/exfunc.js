689fc368fd2c7eccef8cdf35d11b4b8341501f8d
HanSangHa <hansh01@lotte.net>
2020-07-17 14:15:34 +0900

    inner function 외부로 및 매개변수전달

--- src/ch6/exfunc.js
+++ src/ch6/exfunc.js
@@ -1,21 +1,33 @@
 function printOwing(invoice) {
   let outstanding = 0;
-  
-  console.log("***********");
-  console.log("***고객 채무***");
-  console.log("***********");
+
+
+  printBanner();
 
   // 미해결 채무(outstanding)를 계산한다.
   for (const o of invoice.orders) {
     outstanding += o.amount;
   }
 
-  const today = Clock.today;
-
-  invoice.dueDate = new(today.getFullYear(), today.getMonth(),
-                        today.getDate() + 30);
+  recordDueDate(invoice);
 
   // 세부 사항을 출력한다.
+  printDetail(invoice, outstanding);                   
+}
+
+function recordDueDate(invoice){
+  const today = Clock.today;
+  invoice.dueDate = new(today.getFullYear(), today.getMonth(),
+                        today.getDate() + 30);
+}
+
+function printBanner() {
+  console.log("***********");
+  console.log("***고객 채무***");
+  console.log("***********");
+}
+
+function printDetail(invocie, outstanding) {
   console.log(`고객명: ${invoice.customer}`);
   console.log(`채무액: ${outstanding}`);
   console.log(`마감일: ${invocie.dueDate.toLocaleDateString()}`);
