e23f7a7aa3c3eec24304e2f617fd5c7b1b4fd7c4
HanSangHa <hansh01@lotte.net>
2020-07-17 14:46:18 +0900

    inline 예제 2 init

--- src/ch6/inlineFunc.js
+++ src/ch6/inlineFunc.js
@@ -2,3 +2,16 @@
 function rating(aDriver) {
   return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
+
+
+// inline 2 좀 더 복잡한 경우
+function reportLines(aCustomer) {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+}
+
+function gatherCustomerData(out, aCustomer) {
+  out.push(["name", aCustomer.name]);
+  out.push(["location", aCustomer.location]);
+}
